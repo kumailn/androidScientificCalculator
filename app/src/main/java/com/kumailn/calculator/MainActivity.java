7c526e0896219e5988cc192c4061b4933e62b895
Kumail <kumailmn@gmail.com>
2017-06-18 06:32:16 -0400

    Added support for commas as decimal points.

--- app/src/main/java/com/kumailn/calculator/MainActivity.java
+++ app/src/main/java/com/kumailn/calculator/MainActivity.java
@@ -1110,6 +1110,10 @@ public class MainActivity extends AppCompatActivity {
         TextView instantCalcView = (TextView)findViewById(R.id.instantCalcluationView);
         TextView pView = (TextView)findViewById(R.id.primeView);
 
+        if(currentCalculation.contains(",") && (!(currentCalculation.contains("lcm")) && !(currentCalculation.contains("gcd")))){
+            currentCalculation = currentCalculation.replace(",", ".");
+        }
+
         try {
             //currentCalculation = currentCalculation.replace("Ans", previousAns);
             //displayCalculation = displayCalculation.replace("Ans", previousAns);
