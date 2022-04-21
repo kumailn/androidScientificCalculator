2492ee883ab7b2e35a2ef6ccae4c6e2a70079651
JuanGomezGlb <44448286+JuanGomezGlb@users.noreply.github.com>
2022-04-21 12:18:02 -0300

    Update ExampleUnitTest.java

--- app/src/test/java/com/kumailn/calculator/ExampleUnitTest.java
+++ app/src/test/java/com/kumailn/calculator/ExampleUnitTest.java
@@ -12,6 +12,55 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
+        assertEquals(1, 0 + 1);
+        assertEquals(2, 1 + 1);
+        assertEquals(3, 1 + 2);
         assertEquals(4, 2 + 2);
+        assertEquals(5, 3 + 2);
+        assertEquals(6, 3 + 3);
+        assertEquals(7, 4 + 3);
+        assertEquals(8, 4 + 4);
+        assertEquals(9, 5 + 4);
+        assertEquals(10, 5 + 5);
+    }
+    
+        @Test
+    public void addition_isCorrect2() throws Exception {
+        assertEquals(1, 0 + 1);
+        assertEquals(2, 1 + 1);
+        assertEquals(3, 1 + 2);
+        assertEquals(4, 2 + 2);
+        assertEquals(5, 3 + 2);
+        assertEquals(6, 3 + 3);
+        assertEquals(7, 4 + 3);
+        assertEquals(8, 4 + 4);
+        assertEquals(9, 5 + 4);
+        assertEquals(10, 5 + 5);
+    }
+        @Test
+    public void addition_isCorrect3() throws Exception {
+        assertEquals(1, 0 + 1);
+        assertEquals(2, 1 + 1);
+        assertEquals(3, 1 + 2);
+        assertEquals(4, 2 + 2);
+        assertEquals(5, 3 + 2);
+        assertEquals(6, 3 + 3);
+        assertEquals(7, 4 + 3);
+        assertEquals(8, 4 + 4);
+        assertEquals(9, 5 + 4);
+        assertEquals(10, 5 + 5);
+    }
+        @Test
+    public void addition_isCorrect4() throws Exception {
+        assertEquals(1, 0 + 1);
+        assertEquals(2, 1 + 1);
+        assertEquals(3, 1 + 2);
+        assertEquals(4, 2 + 2);
+        assertEquals(5, 3 + 2);
+        assertEquals(6, 3 + 3);
+        assertEquals(7, 4 + 3);
+        assertEquals(8, 4 + 4);
+        assertEquals(9, 5 + 4);
+        assertEquals(10, 5 + 5);
     }
 }
